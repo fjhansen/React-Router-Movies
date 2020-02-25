12f7bab5e9feceaba1f222183f89f21080d4aa72
Frances Hansen <francesjuniperhansen@gmail.com>
2020-02-25 15:40:45 -0800

    mvp before stretch

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link, NavLink } from 'react-router-dom'
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -21,7 +22,9 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <NavLink to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </NavLink>
       ))}
     </div>
   );
@@ -30,6 +33,7 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+    
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -46,6 +50,7 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+    
   );
 }
 
